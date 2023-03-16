db631d0283f0243b330d67f983fc6db5386fbcdc
jonathadev32 <jonathadev32@gmail.com>
2023-03-15 22:41:14 -0300

    commit inicial loja virtual jhon

---
+++ src/main/java/jhondev/lojavirtual/LojaVirtualJhonApplication.java
@@ -0,0 +1,13 @@
+package jhondev.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualJhonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualJhonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jhondev/lojavirtual/LojaVirtualJhonApplicationTests.java
@@ -0,0 +1,13 @@
+package jhondev.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualJhonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
